32779194f6c0a0cab585f2640d24727b593f420a
Wonakiri <126415289+Wonakiri@users.noreply.github.com>
2023-10-10 18:44:00 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ cd /tmp
 rm -r logscan
 curl https://raw.githubusercontent.com/Wonakiri/logscan/main/logscan > logscan
 
-cd ~/bin/logscan-files
+cd ~/bin
 rm -r logscan
 
-mv /tmp/logscan ~/bin/logscan-files
+mv /tmp/logscan ~/bin/
 exit 0
